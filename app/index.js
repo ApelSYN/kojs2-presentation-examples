d73a96259c9c919ef9c5b244bc46edbbfbbb22a4
Oleg cherniy <oleg.cherniy@gmail.com>
2016-08-05 20:51:28 +0300

    add product model

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import Koa from 'koa';
 import config from  'config';
 import err from './middleware/error';
+import routes from './routes';
 
 const app = new Koa();
 
 app.use(err);
 
-require('./routes')(app);
+routes(app);
 
 app.listen(config.server.port, function () {
     console.log('%s listening at port %d', config.app.name, config.server.port);

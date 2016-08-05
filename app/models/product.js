a5c1293d972aa11e291e21cd1aea1db33d23e124
Oleg cherniy <oleg.cherniy@gmail.com>
2016-08-05 22:38:45 +0300

    fix await bugs

--- app/models/product.js
+++ app/models/product.js
@@ -10,15 +10,21 @@ const crud = {
             product = products[0];
         return product;
     },
-    create: async function ({ id = null, name, price = 0, currency = 'UAH' }) {
-        let product = {id: id, name: String(name), price: Number(price), currency: String(currency)};
-        let result = await query(`INSERT INTO ${productTableName} SET ?`,[product]);
+    create: async function ({ id, name, price = 0, currency = 'UAH' }) {
+        let product = {name: String(name), price: Number(price), currency: String(currency)};
+        if (Number(id) > 0) product.id = id;
+        let result = await query(`INSERT INTO ${productTableName} SET ? ON DUPLICATE KEY UPDATE ?`,[product,product]);
         return crud.get(result.insertId);
     },
-    update: async function ( id, { name, price = 0, currency = 'UAH' }) {
-        let product = {name: String(name), price: Number(price), currency: String(currency)};
-        let result = await query(`UPDATE ${productTableName} SET ? WHERE id=?`,[product, Number(id)]);
-        return result.affectedRows;
+    update: async function ( id, product) {
+        if (typeof product === 'object') {
+            let uProduct = {};
+            if (product.hasOwnProperty('name')) uProduct.name = String(product.name);
+            if (product.hasOwnProperty('price')) uProduct.price = Number(product.price);
+            if (product.hasOwnProperty('currency')) uProduct.currency = Number(product.currency);
+            let result = await query(`UPDATE ${productTableName} SET ? WHERE id=?`,[uProduct, Number(id)]);
+            return result.affectedRows;
+        }
     },
     delete: async (id) => {
         let result = await query(`DELETE FROM ${productTableName} WHERE id=?`,[id]);

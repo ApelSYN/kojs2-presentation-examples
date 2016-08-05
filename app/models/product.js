b62828a3798d0af0d33db6b2933f859b708b1566
Oleg cherniy <oleg.cherniy@gmail.com>
2016-08-05 21:36:31 +0300

    fix await bugs

--- app/models/product.js
+++ app/models/product.js
@@ -6,26 +6,26 @@ export default {
         return query(`SELECT * from ${productTableName}`);
     },
     get: async (id) => {
-        let products = await query(`SELECT * from ${productTableName} where id=?`,[id]),
+        let products = await query(`SELECT * FROM ${productTableName} WHERE id=?`,[id]),
             product = products[0];
         return product;
     },
     create: async function ({ name, price = 0, currency = 'UAH' }) {
         let product = {name: String(name), price: Number(price), currency: String(currency)};
-        let result = await query(`INSERT into ${productTableName} SET ?`,[product]);
+        let result = await query(`INSERT INTO ${productTableName} SET ?`,[product]);
         console.log(result);
         product.id = result.insertId;
         return product;
     },
     update: async function ( id, { name, price = 0, currency = 'UAH' }) {
-        let product = {id: Number(id), name: String(name), price: Number(price), currency: String(currency)};
-        let result = await query(`INSERT into ${productTableName} SET ?`,[product]);
+        let product = {name: String(name), price: Number(price), currency: String(currency)};
+        let result = await query(`UPDATE ${productTableName} SET ? WHERE id=?`,[product, Number(id)]);
         console.log(result);
         product.id = result.insertId;
         return product;
     },
     delete: async (id) => {
-        let result = await query(`DELETE from ${productTableName} where id=?`,[id]);
+        let result = await query(`DELETE FROM ${productTableName} WHERE id=?`,[id]);
         console.log(result);
         return result;
     }

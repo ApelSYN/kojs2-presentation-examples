bc6749ab7f058ad4a92343b38599774fd946e200
Oleg cherniy <oleg.cherniy@gmail.com>
2016-08-06 12:22:16 +0300

    final version

--- app/index.js
+++ app/index.js
@@ -1,13 +1,13 @@
 import Koa from 'koa';
 import config from  'config';
 import err from './middleware/error';
-import router from './middleware/routes';
+import routes from './middleware/routes';
 
 const app = new Koa();
 
 app.use(err);
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(routes.routes());
+app.use(routes.allowedMethods());
 
 app.listen(config.server.port, function () {
     console.log('%s listening at port %d', config.app.name, config.server.port);

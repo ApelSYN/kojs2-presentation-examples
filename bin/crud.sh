f2e592063dcc7b268c2bd53cb91daa0c54e7dcb3
Oleg cherniy <oleg.cherniy@gmail.com>
2016-08-06 15:45:42 +0300

    final version

--- bin/crud.sh
+++ bin/crud.sh
@@ -1,3 +1,6 @@
-curl -XPOST "127.0.0.1:3001/product" -d '{"name":"Test1","price":1}' -H 'Content-Type: application/json'
-curl -XPOST "127.0.0.1:3001/product" -d '{"name":"Test2","price":2}' -H 'Content-Type: application/json'
-curl -XPOST "127.0.0.1:3001/product" -d '{"name":"Test3","price":3}' -H 'Content-Type: application/json'
+curl -XPOST "127.0.0.1:3001/product" -d '{"id":1,"name":"Test","price":1}' -H 'Content-Type: application/json'
+curl -XGET "127.0.0.1:3001/product" -H 'Content-Type: application/json'
+curl -XPUT "127.0.0.1:3001/product/1" -d '{"name":"Test1"}' -H 'Content-Type: application/json'
+curl -XGET "127.0.0.1:3001/product/1" -H 'Content-Type: application/json'
+curl -XDELETE "127.0.0.1:3001/product/1"
+

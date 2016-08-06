8dbe80e550c354f8f84c8e7843ad2ce9b4d270cb
Oleg cherniy <oleg.cherniy@gmail.com>
2016-08-06 13:24:51 +0300

    final version

--- app/models/product.js
+++ app/models/product.js
@@ -1,18 +1,19 @@
 import query from 'mysql-query-promise';
-const productTableName = 'products';
+import config from  'config';
+const tableName = config.product.tableName;
 
 const crud = {
     getAll: async () => {
-        return query(`SELECT * from ${productTableName}`);
+        return query(`SELECT * from ${tableName}`);
     },
     get: async (id) => {
-        let products = await query(`SELECT * FROM ${productTableName} WHERE id=?`,[id]);
+        let products = await query(`SELECT * FROM ${tableName} WHERE id=?`,[id]);
         return products[0];
     },
     create: async function ({ id, name, price = 0, currency = 'UAH' }) {
         let product = {name: String(name), price: Number(price), currency: String(currency)};
         if (Number(id) > 0) product.id = id;
-        let result = await query(`INSERT INTO ${productTableName} SET ? ON DUPLICATE KEY UPDATE ?`,[product,product]);
+        let result = await query(`INSERT INTO ${tableName} SET ? ON DUPLICATE KEY UPDATE ?`,[product,product]);
         if (result.insertId) id = result.insertId;
         return crud.get(id);
     },
@@ -22,12 +23,12 @@ const crud = {
             if (product.hasOwnProperty('name')) uProduct.name = String(product.name);
             if (product.hasOwnProperty('price')) uProduct.price = Number(product.price);
             if (product.hasOwnProperty('currency')) uProduct.currency = String(product.currency);
-            let result = await query(`UPDATE ${productTableName} SET ? WHERE id=?`,[uProduct, Number(id)]);
+            let result = await query(`UPDATE ${tableName} SET ? WHERE id=?`,[uProduct, Number(id)]);
             return result.affectedRows;
         }
     },
     delete: async (id) => {
-        let result = await query(`DELETE FROM ${productTableName} WHERE id=?`,[id]);
+        let result = await query(`DELETE FROM ${tableName} WHERE id=?`,[id]);
         return result.affectedRows;
     }
 };
--- config/default.js
+++ config/default.js
@@ -6,6 +6,9 @@ module.exports = {
     server: {
         port: 3001
     },
+    product: {
+        tableName: 'products'
+    },
     database: {
         master: {
             host: "localhost",
